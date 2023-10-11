5e929c4a0d0d7005a7cda49aa5bfc2291204db76
Aarthireddybg <147656409+Aarthireddybg@users.noreply.github.com>
2023-10-12 00:22:09 +0530

    Add files via upload

---
+++ adminVerify.py
@@ -0,0 +1,8 @@
+username='aarthi'
+password='aarthi'
+
+def verifyAdminAccount(usernameEntered,passwordEntered):
+    if usernameEntered==username and passwordEntered==password:
+        return True
+    else:
+        return False
